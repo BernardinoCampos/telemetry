3883ffb5c4b7b7f618a4ac4f10f6d23fa09dd628
bcampos <bcampos@pobox.com>
2021-12-09 19:13:08 +0000

    First release

--- telemetry.py
+++ telemetry.py
@@ -22,14 +22,16 @@ def main():
     mqtt.username_pw_set(username=config['default']['user'], password=config['default']['passwd'])
     mqtt.connect(config['default']['broker'],int(config['default']['port']))
 
-    if os.path.isfile('/sys/class/thermal/thermal_zone0/temp'):
+    if os.path.isfile('/sys/class/thermal/thermal_zone1/temp'):
+        with open('/sys/class/thermal/thermal_zone1/temp') as f:
+            temperature = int(f.read())/1000
+            topic = config['default']['rootTopic']+"/temperature"
+            mqtt.publish(topic.replace('{HOSTNAME}',platform.node()),temperature)
+    elif os.path.isfile('/sys/class/thermal/thermal_zone0/temp'):
         with open('/sys/class/thermal/thermal_zone0/temp') as f:
             temperature = int(f.read())/1000
             topic = config['default']['rootTopic']+"/temperature"
             mqtt.publish(topic.replace('{HOSTNAME}',platform.node()),temperature)
-    elif os.path.isfile('/usr/bin/sensors'):
-        result = subprocess.run(['/usr/bin/sensors'], stdout=subprocess.PIPE)
-        print(result.stdout)
 
 if __name__ == "__main__":
     main()
