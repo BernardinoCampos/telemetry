6aa3369cbea9649c8883c8f506631158fcd3be6f
bcampos <bcampos@pobox.com>
2021-12-09 16:56:26 +0000

    First release

--- telemetry.py
+++ telemetry.py
@@ -2,7 +2,7 @@
 
 def main():
     with open('/sys/class/thermal/thermal_zone0/temp') as f:
-        temperature = f.read()/100;
+        temperature = int(f.read())/100
         print (temperature)
 
 if __name__ == "__main__":
