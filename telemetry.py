92b8e9630365ff7fb051b6e2c68d0bef8f53aee1
bcampos <bcampos@pobox.com>
2021-12-09 17:14:04 +0000

    First release

--- telemetry.py
+++ telemetry.py
@@ -5,16 +5,16 @@ import platform
 def main():
     config = configparser.ConfigParser()
     config.read('/etc/telemetry/telemetry.ini')
-    broker = config['DEFAULT']['broker'];
+    broker = config['default']['broker'];
 
     mqtt = paho.Client("telemetry")
 
-    mqtt.username_pw_set(username=config['DEFAULT']['user'], password=config['DEFAULT']['passwd'])
-    mqtt.connect(config['DEFAULT']['broker'],config['DEFAULT']['port'])
+    mqtt.username_pw_set(username=config['default']['user'], password=config['default']['passwd'])
+    mqtt.connect(config['default']['broker'],config['default']['port'])
 
     with open('/sys/class/thermal/thermal_zone0/temp') as f:
         temperature = int(f.read())/1000
-        topic = config['DEFAULT']['rootTopic']+"/temperature"
+        topic = config['default']['rootTopic']+"/temperature"
         topic.replace('{HOSTNAME}',platform.node())
         mqtt.publish(topic,temperature)
 
