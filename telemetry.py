52936da885f356c3055d2554b8f6924246828b54
bcampos <bcampos@pobox.com>
2024-06-05 00:59:53 +0100

    First release

--- telemetry.py
+++ telemetry.py
@@ -23,24 +23,28 @@ def main():
     mqtt.username_pw_set(username=config['default']['user'], password=config['default']['passwd'])
     mqtt.connect(config['default']['broker'],int(config['default']['port']))
 
-    topic = config['default']['rootTopic'] + "/temperature"
+    topicTemp = config['default']['rootTopic'] + "/temperature"
+    topicLastSeen = config['default']['rootTopic'] + "/last_seen"
 
     if os.path.isfile('/sys/class/thermal/thermal_zone1/temp'):
         print("Opcao 1")
         with open('/sys/class/thermal/thermal_zone1/temp') as f:
             temperature = int(f.read())/1000
-            mqtt.publish(topic.replace('{HOSTNAME}',platform.node()),temperature)
+            mqtt.publish(topicTemp.replace('{HOSTNAME}',platform.node()),temperature)
     elif os.path.isfile('/sys/class/thermal/thermal_zone0/temp'):
         print("Opcao 2")
         with open('/sys/class/thermal/thermal_zone0/temp') as f:
             temperature = int(f.read())/1000
-            mqtt.publish(topic.replace('{HOSTNAME}',platform.node()),temperature)
+            mqtt.publish(topicTemp.replace('{HOSTNAME}',platform.node()),temperature)
     elif platform.system()=='FreeBSD':
         command = subprocess.check_output("sysctl -a | grep temperature | grep 'dev.cpu.' | sort -k2 -r | head -1 | cut -d' ' -f 2",shell=True)
         m = re.match('[\.0-9]+', command.decode())
-        mqtt.publish(topic.replace('{HOSTNAME}', platform.node()), float(m.group(0)))
+        mqtt.publish(topicTemp.replace('{HOSTNAME}', platform.node()), float(m.group(0)))
     else:
         print("Não foi encontrada forma de pegar a temperatura")
+
+
+    mqtt.publish(topicLastSeen.replace('{HOSTNAME}', platform.node()), Date.now())
 
 if __name__ == "__main__":
     main()
